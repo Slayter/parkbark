2d0ac7857182d0314575bf953fe4af3a7d58ea08
Dev1 <dev@gmail.com>
2018-05-29 10:12:10 +0200

    added index files

---
+++ app/components/form_generator/index.js
@@ -0,0 +1,17 @@
+
+
+import {InputField} from './fields/InputField';
+import {SwitchField} from './fields/SwitchField';
+import {Separator} from './fields/Separator';
+import {LinkField} from './fields/LinkField';
+
+import {DatePickerField} from './fields/DatePickerField';
+import {Form} from './Form';
+
+import {KeyboardAwareScrollView} from './KeyboardAwareScrollView';
+//import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view'
+export {InputField, SwitchField,
+        Separator, LinkField,
+        KeyboardAwareScrollView,
+        Form
+      }
---
+++ index.js
@@ -0,0 +1,3 @@
+import { AppRegistry } from 'react-native';
+import Main from "./app/index";
+AppRegistry.registerComponent('parkbark', () => Main);
