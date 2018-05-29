481861717e005246b33a18f3b78366eb10b4a4ea
Dev1 <dev@gmail.com>
2018-05-29 09:51:18 +0200

    updated gradle version

--- android/app/src/main/java/com/parkbark/MainApplication.java
+++ android/app/src/main/java/com/parkbark/MainApplication.java
@@ -1,51 +1,57 @@
 package com.parkbark;
 
 import android.app.Application;
-import android.util.Log;
 
-import com.sbugert.rnadmob.RNAdMobPackage;
 import com.facebook.react.ReactApplication;
-import com.idehub.Billing.InAppBillingBridgePackage;
+import com.sbugert.rnadmob.RNAdMobPackage;
 import cl.json.RNSharePackage;
 import com.learnium.RNDeviceInfo.RNDeviceInfo;
+import com.idehub.Billing.InAppBillingBridgePackage;
+import com.showlocationservicesdialogbox.LocationServicesDialogBoxPackage;
 import com.airbnb.android.react.maps.MapsPackage;
-import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
+import com.facebook.soloader.SoLoader;
 
 import java.util.Arrays;
 import java.util.List;
-import com.showlocationservicesdialogbox.LocationServicesDialogBoxPackage;
-
-
 
 public class MainApplication extends Application implements ReactApplication {
 
-
-
   private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
     @Override
-    protected boolean getUseDeveloperSupport() {
+    public boolean getUseDeveloperSupport() {
       return BuildConfig.DEBUG;
     }
 
     @Override
     protected List<ReactPackage> getPackages() {
-      return Arrays.asList(
+      return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new InAppBillingBridgePackage(),
             new RNSharePackage(),
-            new RNAdMobPackage(),
             new RNDeviceInfo(),
-            new MapsPackage(),
-            new LocationServicesDialogBoxPackage()
+            new InAppBillingBridgePackage(),
+            new LocationServicesDialogBoxPackage(),
+            new RNAdMobPackage(),
+            new MapsPackage()
       );
+    }
+
+    @Override
+    protected String getJSMainModuleName() {
+      return "index";
     }
   };
 
   @Override
   public ReactNativeHost getReactNativeHost() {
-      return mReactNativeHost;
+    return mReactNativeHost;
+  }
+
+  @Override
+  public void onCreate() {
+    super.onCreate();
+    SoLoader.init(this, /* native exopackage */ false);
   }
 }
