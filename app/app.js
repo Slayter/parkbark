294f3f3974fb5960ea99f369e5bfb5739e1dc034
Dev1 <dev@gmail.com>
2018-06-13 09:02:11 +0200

    cleaned app.js, index.js

--- app/app.js
+++ app/app.js
@@ -1,20 +1,18 @@
 import React, { Component } from 'react';
-import { connect } from 'react-redux';
 import Navigator from './navigation/Navigator';
 
-
-
-
-
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    };
+  }
 
   render() {
     return (
-          <Navigator />
+      <Navigator />
     );
   }
-};
-
-
+}
 
 export default App;
--- app/index.js
+++ app/index.js
@@ -1,29 +1,26 @@
 import React, { Component } from 'react';
+import { Provider } from 'react-redux';
 import App from './app';
-import {Provider} from 'react-redux';
 import store from './redux/store';
 import Geolocator from './components/map/Geolocator';
-import InAppBilling from 'react-native-billing';
 import AdsPurchasedCheck from './components/ads/AdsPurchasedChecker';
 
 
 const geolocator = new Geolocator(store);
-const adsPurchsedCheck = new AdsPurchasedCheck(store)
+const adsPurchsedCheck = new AdsPurchasedCheck(store);
 
 const AppProvider = (
-    <Provider store={store}>
-      <App />
-    </Provider>
+  <Provider store={store}>
+    <App />
+  </Provider>
 );
 
 
 class Main extends Component {
-
-
   render() {
     return (
-        AppProvider
-    )
+      AppProvider
+    );
   }
 }
 
