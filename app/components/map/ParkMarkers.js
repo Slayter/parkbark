bb7567a4c1d4ae43cd5e34d469d2657337f77ade
dev <upworkvdvjekanavi39@gmail.com>
2018-08-13 11:37:04 +0200

    updated Map tooltip

--- app/components/map/ParkMarkers.js
+++ app/components/map/ParkMarkers.js
@@ -6,7 +6,24 @@ import {
 	StyleSheet, Dimensions
 } from 'react-native';
 import { connect } from 'react-redux';
+import { isEqual } from 'lodash';
 import MapView, { Callout } from 'react-native-maps';
+import Svg,{
+	Circle,
+	Ellipse,
+	G,
+	LinearGradient,
+	RadialGradient,
+	Line,
+	Path,
+	Polygon,
+	Polyline,
+	Rect,
+	Symbol,
+	Use,
+	Defs,
+	Stop
+} from 'react-native-svg';
 import { Actions } from 'react-native-router-flux';
 import { googledistanceapi } from '../../api/googleapi.js';
 import { getDistance } from '../../src/map_core';
@@ -14,17 +31,17 @@ import { getDistance } from '../../src/map_core';
 const { width, height } = Dimensions.get('window');
 
 const styles = StyleSheet.create({
-	callout: {
-		backgroundColor: '#fff',
-		padding: 2,
-		borderRadius: 2,
-		position: 'relative',
-		alignItems: 'center',
-		elevation: 4,
-		shadowColor: 'rgba(0,0,0,.24)',
-		shadowOffset: { width: 0, height: 2 },
-		shadowRadius: 4,
-	},
+	// callout: {
+	// 	backgroundColor: '#fff',
+	// 	padding: 2,
+	// 	borderRadius: 2,
+	// 	position: 'relative',
+	// 	alignItems: 'center',
+	// 	elevation: 4,
+	// 	shadowColor: 'rgba(0,0,0,.24)',
+	// 	shadowOffset: { width: 0, height: 2 },
+	// 	shadowRadius: 4,
+	// },
 	top: {
 		flexDirection: 'row',
 		justifyContent: 'space-between',
@@ -53,112 +70,154 @@ const styles = StyleSheet.create({
 	  alignSelf: 'center',
 		width: 0,
 		height: 0,
-		backgroundColor: 'transparent',
+		backgroundColor: 'black',
 		borderStyle: 'solid',
-		borderLeftWidth: 5,
-		borderRightWidth: 5,
-		borderBottomWidth: 8,
-		borderLeftColor: 'transparent',
-		borderRightColor: 'transparent',
-		borderBottomColor:"white",
+		borderLeftWidth: 15,
+		borderRightWidth: 15,
+		borderBottomWidth: 18,
+		borderLeftColor: 'red',
+		borderRightColor: 'blue',
+		borderBottomColor: '#DBDBDB',
 		transform: [
 			{ rotate: '180deg' }
 		],
 		margin: 0,
 		marginBottom: 5,
 		borderWidth: 0,
-		borderColor:"transparent"
+		borderColor: 'steelblue'
+	},
+	tooltipMainContainer: {
+		backgroundColor: 'white',
+		borderRadius: 4,
+		padding: 10,
+		borderColor: '#DBDBDB',
+		borderWidth: 1,
+	},
+	tooltipSubContainer: {
+		flexDirection: 'row',
+		justifyContent: 'space-between',
+	},
+	tooltipTitleText: {
+		color: 'black',
+		fontSize: 16,
+		fontFamily: 'Source Sans Pro 700',
+		maxWidth: (width / 4) * 3
+	},
+	tooltipDescriptionText: {
+		color: '#F58120',
+		fontSize: 14,
+		marginLeft: 10,
+		fontFamily: 'Source Sans Pro regular',
+		marginTop: 1
+	},
+	tooltipDescriptionText1: {
+		color: '#6E6E6E',
+		fontSize: 14,
+		fontFamily: 'Source Sans Pro regular',
+		maxWidth: (width / 4) * 3
+	},
+	calloutSubContainer: {
+		position: 'absolute',
+		left: 0,
+		right: 0,
+		bottom: 9,
+		height: 13,
+		justifyContent: 'flex-end',
+		alignItems: 'center',
+		backgroundColor: 'transparent'
+	},
+	subContainer1: {
+		width: 13,
+		height: 1,
+		backgroundColor: 'white'
+	},
+	subContainer2: {
+		width: 15,
+		height: 1.5,
+		backgroundColor: 'white'
 	}
 });
 
 class ParkMarkers extends Component {
   constructor() {
     super();
-    this.onCalloutPress.bind(this);
     this.selectedMarker = {};
-    this.state = { selected: null };
+    this.state = {
+    	selected: null,
+			markers: []
+    };
   }
+  
+  componentWillReceiveProps(nextProps) {
+		if (!isEqual(this.props.markers, nextProps.markers)) {
+			this.setState({ markers: nextProps.markers }, () => {
+				if (this.state.selected !== null && this.selectedMarker[this.state.selected] !== null) {
+					setTimeout(() => this.selectedMarker[this.state.selected].showCallout(), 0);
+				}
+			});
+		}
+	}
+  
+  onPress = (marker) => {
+		this.setState({ selected: marker.title });
+	};
 
-  componentDidUpdate() {
-    if (this.state.selected !== null && this.selectedMarker[this.state.selected] !== null) {
-      this.selectedMarker[this.state.selected].showCallout();
-    }
-  }
-
-  onCalloutPress(title) {
+  onCalloutPress = (title) => {
     this.props.dispatch({type: 'UPDATE_SElECTED_PARK', state: title});
     // this.props.navigator.push({name:'parkdetail'});
     Actions.parkdetail();
-  }
+  };
 
   render() {
+  	const { markers } = this.state;
     const default_position = {
       latitude: 45.523031,
       longitude: -122.676772
     };
-    // const position = this.props.coords || default_position;
-    const position = default_position;
-
+    const position = this.props.coords || default_position;
+    // const position = default_position;
     return (
       <View>
-        {this.props.markers.map((marker, i) =>
+        {markers.map((marker, i) =>
           <MapView.Marker
             ref={(ref) => { this.selectedMarker[marker.title] = ref; }}
-            onPress={() => this.setState({ selected: marker.title })}
-            key={marker.title + i}
+            onPress={() => this.onPress(marker)}
+						key={i + marker.title}
             coordinate={marker.latlng}
             image={require('../../img/map-pin.png')}
-            title={marker.title}
-            description={marker.address_display + ' apx. ' + getDistance(position.latitude, position.longitude, marker.latlng.latitude, marker.latlng.longitude) + ' mi'}
-            onCalloutPress={() => this.onCalloutPress(marker.title)}
-          >
-            <Callout tooltip={true} style={{ elevation: 4 }}>
-              <View style={{ elevation: 4 }}>
-                <View style={{ backgroundColor: 'white', padding: 10, borderRadius: 5, elevation: 4 }}>
-                  <View style={{ flex: 1 }}>
-										<View
-											style={{
-												flexDirection: 'row',
-												justifyContent: 'space-between',
-												alignItems: 'center',
-												marginBottom: 5
-											}}
-										>
-											<Text
-												style={{
-													color: 'black',
-													fontSize: 16,
-													fontWeight: 'bold',
-													fontFamily: 'Source Sans Pro regular',
-												}}
-											>
-												{marker.title}
-											</Text>
-											<Text
-												style={{
-													color: '#F58120',
-													fontSize: 14,
-													marginLeft: 20,
-													fontFamily: 'Source Sans Pro regular',
-												}}
-											>
-												{`${getDistance(position.latitude, position.longitude, marker.latlng.latitude, marker.latlng.longitude)} mi`}
-											</Text>
-										</View>
-                  </View>
-									<Text
-                    style={{
-                      color: '#6E6E6E',
-                      fontSize: 14,
-											fontFamily: 'Source Sans Pro regular',
-                    }}
-                  >
+						onCalloutPress={() => this.onCalloutPress(marker.title)}
+					>
+            <MapView.Callout tooltip={true}>
+							<View>
+								<View style={styles.tooltipMainContainer}>
+									<View style={styles.tooltipSubContainer}>
+										<Text style={styles.tooltipTitleText} numberOfLines={2}>
+											{marker.title}
+										</Text>
+										<Text style={styles.tooltipDescriptionText}>
+											{`${getDistance(position.latitude, position.longitude, marker.latlng.latitude, marker.latlng.longitude)} mi`}
+										</Text>
+									</View>
+									<Text style={styles.tooltipDescriptionText1} numberOfLines={2}>
 										{marker.address_display}
 									</Text>
-                </View>
-								<View style={styles.triangle} />
-              </View>
-            </Callout>
+								</View>
+								<View style={{ alignItems: 'center', justifyContent: 'center' }}>
+									<Svg height="10" width="16">
+										<Polygon
+											points="16,0 0,0 8,10"
+											fill="white"
+											stroke="#DBDBDB"
+											strokeWidth="1"
+										/>
+									</Svg>
+								</View>
+							</View>
+							<View style={styles.calloutSubContainer}>
+								<View style={styles.subContainer2} />
+								<View style={styles.subContainer1} />
+							</View>
+            </MapView.Callout>
           </MapView.Marker>)}
       </View>
     );
