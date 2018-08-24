e61905f89fbe9534a7ca1ddc18cdb5c51906dc13
dev <upworkvdvjekanavi39@gmail.com>
2018-08-24 10:38:10 +0200

    fixed the issue for listener of network connection

--- app/components/common/NetworkAlert.js
+++ app/components/common/NetworkAlert.js
@@ -5,28 +5,42 @@ import { NetInfo, Alert, BackHandler } from 'react-native';
 class NetworkAlert {
   constructor() {
     this.isConnected = null;
-    this._handleConnectionInfoChange = (connectionInfo) => {
-      this.isConnected = connectionInfo;
-      if (this.isConnected === false || this.isConnected === 'NONE') {
-        Alert.alert(
-          'Network Offline',
-          'Sorry, but WIFI is necessary to use our app',
-          [
-            { text: 'OK', onPress: () => BackHandler.exitApp() },
-          ],
-          {
-            cancelable: false
-          }
-        );
-      }
-    };
-    NetInfo.addEventListener('change', this._handleConnectionInfoChange);
+    this.timeOut = null;
+    NetInfo.addEventListener('connectionChange', this._handleConnectionInfoChange);
   }
+	
+  _handleConnectionInfoChange = (connectionInfo) => {
+	  this.isConnected = connectionInfo.type !== 'none';
+	  if (!this.isConnected) {
+		  if (!this.timeOut) {
+			  this.timeOut = setTimeout(() => {
+				  clearTimeout(this.timeOut);
+				  this.timeOut = 0;
+				  Alert.alert(
+					  'Network Offline',
+					  'Sorry, but WIFI is necessary to use our app',
+					  [
+						  { text: 'OK', onPress: () => BackHandler.exitApp() },
+					  ],
+					  {
+						  cancelable: false
+					  }
+				  );
+			  }, 1000 * 60 * 3);
+		  }
+	  } else {
+		  clearTimeout(this.timeOut);
+		  this.timeOut = 0;
+	  }
+	};
+
   checkConnection() {
     NetInfo.isConnected.fetch().then(isConnected => {
       this.isConnected = isConnected;
       if (this.isConnected === false) {
-        Alert.alert(
+      	clearTimeout(this.timeOut);
+      	this.timeOut = 0;
+      	Alert.alert(
           'Network Offline',
           'Sorry, but WIFI is necessary to use our app',
           [
